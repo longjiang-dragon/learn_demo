086fc066b695fbca3a6b4b736a6e283d663c6080
蒋龙 <jianglong@u51.com>
2018-04-08 11:02:02 +0800

    线程池修改

--- app/src/main/java/com/hujiang/mytest/fragment/manager/Executor/AsyncThreadExecutor.java
+++ app/src/main/java/com/hujiang/mytest/fragment/manager/Executor/AsyncThreadExecutor.java
@@ -2,27 +2,25 @@ package com.hujiang.mytest.fragment.manager.Executor;
 
 import android.support.annotation.NonNull;
 
-import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
  * @author jianglong
- * @desc
+ * @desc  满足任务需要串行执行，且任务一但执行完成，后续就不会再执行
  * @date 2018/4/4
  */
 public class AsyncThreadExecutor extends ThreadPoolExecutor {
-    private static final int CPU_COUNT = Runtime.getRuntime().availableProcessors();
 
     public AsyncThreadExecutor() {
-
-        super(0, CPU_COUNT, 10L, TimeUnit.SECONDS,
-                new SynchronousQueue<Runnable>(), new ThreadFactory() {
+        super(0, 1, 30L, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<Runnable>(), new ThreadFactory() {
                     @Override
                     public Thread newThread(@NonNull Runnable r) {
                         return new Thread(r, "lib-initiation");
                     }
-                }, new ThreadPoolExecutor.CallerRunsPolicy());
+                });
     }
 }

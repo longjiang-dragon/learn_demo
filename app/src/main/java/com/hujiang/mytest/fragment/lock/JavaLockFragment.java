4a6936bf8be7ac8c05319ad8436080cbe4c27ea6
jianglong <jiangl@mallcai.com>
2020-01-10 11:55:13 +0800

    [add]LockSupport测试

--- app/src/main/java/com/hujiang/mytest/fragment/lock/JavaLockFragment.java
+++ app/src/main/java/com/hujiang/mytest/fragment/lock/JavaLockFragment.java
@@ -1,9 +1,14 @@
 package com.hujiang.mytest.fragment.lock;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.hujiang.mytest.util.PrintUtil;
+
+import java.util.concurrent.locks.LockSupport;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -23,7 +28,28 @@ public class JavaLockFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        JavaLockDemo.run2();
 //        ExclusiveLock.run();
-        TwinsLock.run();
+//        TwinsLock.run();
+
+
+
+        //关于线程挂起的测试
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                PrintUtil.printLog(" LockSupport.park(this)start;");
+                LockSupport.park(JavaLockFragment.this);
+                PrintUtil.printLog(" LockSupport.park(this);");
+            }
+        });
+        thread.start();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                LockSupport.unpark(thread);
+            }
+        }, 10000);
+
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 

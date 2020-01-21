d68cc09fae5a31f34a5ee02f0e27d4fc31284dfb
jianglong <jianglong@moretail.cn>
2020-01-21 15:41:36 +0800

    [ADD]二维数组回形打印

--- app/src/main/java/com/hujiang/mytest/fragment/algorithm/AlgorithmFragment.java
+++ app/src/main/java/com/hujiang/mytest/fragment/algorithm/AlgorithmFragment.java
@@ -36,7 +36,7 @@ public class AlgorithmFragment extends Fragment {
         ButterKnife.bind(this, view);
     }
 
-    @OnClick ({R.id.tv_bfs, R.id.tv_dfs, R.id.avl_tree})
+    @OnClick ({R.id.tv_bfs, R.id.tv_dfs, R.id.avl_tree,R.id.avl_print})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.tv_bfs:
@@ -54,8 +54,85 @@ public class AlgorithmFragment extends Fragment {
                 tree.add("2", 1);
                 tree.add("3", 1);
                 break;
+            case R.id.avl_print:
+                //回形打印二维数组
+                int [][] arr = new int[4][5];
+                travel(arr, 4, 5, 0, 0);
+                print(arr, 4, 5);
+                break;
+        }
+    }
+    /**
+     *
+     * 0   1   2   3
+     * 13  14  15  4
+     * 12  19  16  5
+     * 11  18  17  6
+     * 10  9   8   7
+     *
+     * @param arr  数组
+     * @param row  行
+     * @param col  列
+     * @param level 层数
+     * @param count 计数
+     */
+    public void travel(int [][] arr, int row, int col, int level, int count) {
+        //递归出口
+        if (row - 2 * level == 0 || col - 2 * level == 0) {
+            return;
         }
 
+        if (row - 2 * level == 1) {
+            //只有一行了,不用处理四条边的逻辑
+            for (int i = level; i < col -level; ++i) {
+                arr[level][i] = count++;
+            }
+            return;
+        }
 
+        if (col - 2 * level == 1) {
+            //只有一列的场景,不用处理四条边的逻辑
+            for (int i = level; i < row -level; ++i) {
+                arr[i][level] = count++;
+            }
+            return;
+        }
+
+        //上边
+        for (int i = level; i < col - level; ++i) {
+            arr[level][i] = count++;
+        }
+
+        //右边
+        for (int i = level + 1; i < row - 1 - level; ++i) {
+            arr[i][col - 1 - level] = count++;
+        }
+        //下边
+        for (int i = col - 1 - level; i >= level; --i){
+            arr[row - 1- level][i] = count++;
+        }
+        //左边
+        for (int i = row - 2 - level; i >= level+1; --i){
+            arr[i][level] = count++;
+        }
+        // 递归
+        travel(arr, row, col, level + 1, count);
     }
+
+
+    /**
+     * 打印
+     * @param arr
+     * @param row
+     * @param col
+     */
+    public void print(int[][] arr, int row, int col){
+        for (int i = 0; i < row; ++i){
+            for (int j = 0; j < col; ++j){
+                System.out.print(arr[i][j] + "    ");
+            }
+            System.out.println();
+        }
+    }
+
 }
